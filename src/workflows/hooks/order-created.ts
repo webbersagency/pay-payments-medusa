60cd19a750f15aca22d0d8b412476feeffac1fe4
Pepijn van Vlaanderen <pepijn@webbers.com>
2025-11-17 17:54:06 +0100

    Fix import createOrderWorkflow

--- src/workflows/hooks/order-created.ts
+++ src/workflows/hooks/order-created.ts
@@ -1,13 +1,13 @@
 import {MedusaContainer} from "@medusajs/types"
 import createPayOrder from "../../utils/createPayOrder"
+import {createOrderWorkflow} from "@medusajs/core-flows"
 
 /**
  * Pay. needs order information to validate and/or verify with after pay payment
  * solutions. Therefore, Medusa payment sessions are ignored, but then we need to
  * create the Pay. order payment here in the order created flow.
  */
-// @ts-ignore
-createOrdersWorkflow.hooks.orderCreated(
+createOrderWorkflow.hooks.orderCreated(
   async (
     {order, additional_data},
     {container}: {container: MedusaContainer}
